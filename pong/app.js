21be915a47f6d2c1bbd7907440fa2388e5faee22
Michael L Parks <github@wmi-inc.com>
2016-07-28 12:16:25 -0400

    Initial Commit

---
+++ pong/app.js
@@ -0,0 +1,7 @@
+index.html:24 CanvasRenderingContext2D {
+  canvas: canvas#gc,
+  globalAlpha: 1,
+  globalCompositeOperation: "source-over",
+  webkitImageSmoothingEnabled: true,
+  imageSmoothingEnabled: true…
+}
